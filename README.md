65fbe6e482273ad3a6a62d4ce25e958c111e2753
Umesh R Bilagi <umeshbilagi@gmail.com>
2018-03-28 15:35:08 +0530

    express ready

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jwt-login
 
-![verson](https://img.shields.io/badge/version-0.0.2-green.svg)
+![verson](https://img.shields.io/badge/version-0.0.3-green.svg)
 ![License](https://img.shields.io/badge/License-MIT-yellowgreen.svg)
 
 
@@ -16,7 +16,7 @@ under the hood it uses crypto-js
 
 
 ### include in your project
-```
+``` javascript
     var JWT = require("jwt-login")
 ```
 
@@ -24,7 +24,7 @@ under the hood it uses crypto-js
 1. It sets the secrete key. if not set, it uses defualt one
 2. Always set secreate key before createJWT method. Do not change it later in your code
 
-```
+``` javascript
     JWT.setSecretKey();// this sets new secret key 
 ```
 
@@ -34,7 +34,7 @@ under the hood it uses crypto-js
 3. payload argument has to be passed
 4. header argument is optional
 
-```
+``` javascript
     var payload = {"user" : username, "expDate" : Date}
     //header second is optional.
     var token = JWT.createJWT(payload, header)// returns the JWTtoken
@@ -44,7 +44,73 @@ under the hood it uses crypto-js
 1. This returns the payload in case validation was sucusseful.
 2. for unsuccessful validatation it returns false.
 3. Argument is JWT. This JWT token is the one recived from client application.
-```
+``` javascript
     var valid = validateJWT(JWT);
     
+```
+
+### express js example
+``` javascript
+const express = require("express")
+const httpMsgs = require("http-msgs");
+const jwtLogin = require("jwt-login");
+const bodyparser = require("body-parser");
+const app = express();
+
+app.listen(9000);
+
+app.use(bodyparser.urlencoded({extended : false}));
+
+//login html file
+app.get("/login", function(req, res){
+    res.sendFile(__dirname + "/login.html");
+});
+
+//login
+app.post("/login",  function(req, res){
+    var user = req.body.user
+    var password = req.body.password
+    if (user == password){
+        jwtLogin.sign(req, res, user,"topsecret", 1, false);  
+    }else{
+        httpMsgs.send500(req, res, "invalid user");
+    }
+    
+});
+// logout
+app.get("/logout", function(req, res){
+    jwtLogin.signout(req, res, false);
+});
+
+var valid_login = function(req, res, next){
+    try {
+        req.jwt = jwtLogin.validate_login(req, res);
+        next();
+    } catch (error) {
+        httpMsgs.send500(req, res, error);
+        
+    }
+
+}
+
+
+/*
+===============================
+    routes 
+==============================
+
+*/ 
+
+app.get("/article", valid_login, function(req, res){
+    var user = req.jwt.user//this the user 
+    httpMsgs.sendJSON(req, res,{
+        from    : "get"
+    });
+});
+
+app.post("/article", function(req, res){
+    httpMsgs.sendJSON(req, res,{
+        from    : "post"
+    });
+});
 ```
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var curCrypt = require("crypto-js");
 var secretKey='PasswordPradyu';//defualt secret key change this in  your application
+var moment = require("moment"); //for manging date
+var httpMsgs = require("http-msgs");
+
 
 var encode_base64 = function(rawStr) {//converts raw string 
     var wordArray = curCrypt.enc.Utf8.parse(JSON.stringify(rawStr));
@@ -13,12 +16,14 @@ var decode_base64 = function(encStr) {// this decode the base64
 }
 
 
-exports.setSecretKey = function(secret){//setting secrete key
+var setSecretKey = function(secret){//setting secrete key
     /*
         use this method before starting the application
     */ 
     secretKey = secret;
 }
+exports.setSecretKey = setSecretKey;
+
 var createJWT = function (payload, header = {"alg": "HS256", "typ": "JWT" }) {
     var secret = secretKey;// this accesed from modulewide secrete key
     var base64Header = encode_base64(header);
@@ -56,3 +61,70 @@ var validateJWT= function(jwt){
     
 }
 exports.validateJWT = validateJWT;
+
+var sign = function(req, res, user, secret, expireInMinutes=0, https=false){
+    // this creates the jwttoken cookie in client
+    /*
+        user is username
+        secrete is secretkey for jwttoken
+        expireinminutes set jwttoken invalid set period 0 never expires offcouse cookie expire is deffeb=rent
+        https boolen is for check type of serve true create secure cookie
+    */ 
+    if (https == true){
+        var secure  = secure
+    }else{
+        var secure = '';
+    }
+    var curDate = moment();
+   var payload = {"user" : user, "createdDate" : curDate, "expireInMinutes" : expireInMinutes};// creates the payload for JWT
+   setSecretKey(secret);// setting secret key
+   createJWT(payload);//create JWT  with payload
+   var token = httpMsgs.setCookieString(req, res, "JWTtoken",  createJWT(payload),'', 86400,true, secure ); 
+   httpMsgs.setCookie(req, res,token, "Login Successful",true);//res.end() is triggerd by setcooke method 
+
+}
+exports.sign = sign;
+
+var signout = function(req, res, https=false){
+    if (https == true){
+        var secure  = secure
+    }else{
+        var secure = '';
+    }
+    var token = httpMsgs.setCookieString(req, res, "JWTtoken",  "",'', 86400,true, secure );
+    httpMsgs.setCookie(req, res,token, "Loged out",true);//res.end() is triggerd by setcooke method 
+}
+exports.signout = signout;
+
+
+// validate_login middle ware
+var validate_login = function(req, res){
+    // this method varifies the JWT and expire time
+    
+    var JWTtoken = httpMsgs.getCookie(req, res, "JWTtoken");// gets the cookie JWTtoken
+    if(JWTtoken == ""){// not allowed if JWTtoken is undefined
+        //write code for forbidden 
+        throw "Not allowed access this content";
+    }else{
+        var JWTtoken = httpMsgs.getCookie(req, res, "JWTtoken");// gets the cookie JWTtoken
+        var validJWT = validateJWT(JWTtoken);// check the authenticity of JET token
+        if (validJWT == false){// not allowed if invalid
+            //write code for forbidden
+            throw "Not allowed access this content";
+        }else{// valid JWT token then
+            var createdDate = JSON.parse(validJWT).createdDate;// access carted time
+            var expireInMinutes = JSON.parse(validJWT).expireInMinutes; //  access expires durration in minutes
+
+            var ftime = moment(createdDate);// created time
+            var nowTime = moment();// now time 
+            var gapTime = moment(nowTime).diff(ftime, "minutes");// gaptime to compare event from created time
+
+            if ((gapTime > expireInMinutes) && (expireInMinutes > 0 )){
+                // if time si more or if expireInMinutes is set more than zero "zero and less means infinate expire time"
+                return false;
+            }
+           return JSON.parse(validJWT)// passe it route with user name careted time and expire
+        }
+    }
+}    
+exports.validate_login = validate_login;  
